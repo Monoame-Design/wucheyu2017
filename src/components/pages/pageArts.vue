d6c5d8a53a44f48b136d66d87aa93e935be71237
Wu Che Yu <frank890417@gmail.com>
2021-11-22 10:58:56 -0500

    update artwork page

--- src/components/pages/pageArts.vue
+++ src/components/pages/pageArts.vue
@@ -1,30 +1,36 @@
 <template lang="pug">
   .page-art
-    .container-fluid
-      .row.mt-4
+    .container
+      .row.mt-3
         .col-12
+
           h1 Generative Arts
-          pre by Che-Yu Wu 
-          p Keep exploring the world with curious mind and imaginations.
+          //- pre by Che-Yu Wu 
+          p.mb-5 Keep exploring the world with curious mind and imaginations.
           
-            
-          a(href="https://www.instagram.com/bosscodingplease/" target="_blank" title="Instagram").mr-5
-            i.fab.fa-instagram 
-            span  bosscodingplease
-          a(href="https://twitter.com/Majer666666" target="_blank" title="Instagram")
-            i.fab.fa-twitter 
-            span  Twitter
-          a(href="https://foundation.app/@cheyuwu" target="_blank" title="Instagram") 
-            img.icon(src="/static/icon_foundation.jpeg" )
-            span  Foundation
-          a(href="https://opensea.io/account?tab=created" target="_blank" title="Instagram") 
-            img.icon(src="/static/icon_opensea.png" )
-            span  Opensea
+          .row
+            .col-lg-6.text-left
+              input(v-model="keyword" placeholder="Search").d-none.d-lg-block
+            .col-lg-6.text-center 
+              a(href="https://www.instagram.com/bosscodingplease/" target="_blank" title="Instagram").mr-5
+                i.fab.fa-instagram 
+                span  bosscodingplease
+              a(href="https://twitter.com/Majer666666" target="_blank" title="Instagram")
+                i.fab.fa-twitter 
+                span  Twitter
+              a(href="https://foundation.app/@cheyuwu" target="_blank" title="Instagram") 
+                img.icon(src="/static/icon_foundation.jpeg" )
+                span  Foundation
+              a(href="https://opensea.io/account?tab=created" target="_blank" title="Instagram") 
+                img.icon(src="/static/icon_opensea.png" )
+                span  Opensea
           
       .row.mt-4
-        a.col-sm-6.col-md-4.col-lg-3.wow.fadeIn(v-for="(item,itemId) in sketches", 
+        a.col-sm-6.col-md-4.col-lg-3.wow.fadeIn(v-for="(item,itemId) in filteredSketches", 
           :href="`https://www.openprocessing.org/sketch/${item.visualID}`",
-                target="_blank",@mouseenter="hoveringItem=item", @mouseleave="hoveringItem=null")
+          target="_blank",
+          @mouseenter="hoveringItem=item", @mouseleave="hoveringItem=null",
+          :key="item.title" )
           img(:src="( hoveringItem===item && getGifUrl(item))? getGifUrl(item): getThumbnail(item)")
           //img(:src="( hoveringItem===item && getGifUrl(item))? getGifUrl(item): getCacheUrl(getThumbnail(item),item.title)")
           h5.mb-5 \#{{item.title}}
@@ -40,7 +46,8 @@ export default {
   data(){
     return {
       // userData: {}
-      hoveringItem: null
+      hoveringItem: null,
+      keyword: ""
     }
   },
   mounted(){
@@ -66,6 +73,13 @@ export default {
         return results
       }
       return []
+    },
+    filteredSketches(){
+      if (!this.keyword){
+        return this.sketches
+      }else{
+        return this.sketches.filter(sk=>sk.title.toLowerCase().indexOf(this.keyword.toLowerCase())!=-1)
+      }
     }
   },
   methods: {
@@ -86,21 +100,32 @@ export default {
 </script>
 <style lang="sass">
 .page-art 
+  .container
+    max-width: 1700px
   a
     color: black
-    // padding: 15px
+    padding: 5px 10px 
   a h5
     // background-color: black
     // color: white
     color: black
-    margin-top: 10px
-    font-weight: 500
-    font-size: 1.2rem
+    margin-top: 15px
+    // font-weight: 500
+    font-weight: 600
+    font-size: 1.3rem
     text-align: left
   img
     width: 100%
     height: auto
-    box-shadow: 0px 40px 30px -20px rgba(black,0.1)
+    box-shadow: 0px 20px 20px -10px rgba(black,0.1)
+    border-radius: 8px
+    overflow: hidden
+  input
+    width: 100%
+    max-width: 300px
+    border: solid 1px rgba(black,0.3)
+    padding: 5px 20px
+    border-radius: 500px
   .icon
     filter: saturate(0)
     height: 25px
